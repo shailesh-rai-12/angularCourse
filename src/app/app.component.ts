68eff3072ff3e225c72246821f2fc4c8fe5e1d2d
Shailesh <shaileshrai1001@gmail.com>
2021-10-13 17:45:50 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+})
+export class AppComponent {
+  serverElements = [{name:'TestServer',type:'server',content:'Just a test'}];
+
+  onAddServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
+
+  onAddBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+}
